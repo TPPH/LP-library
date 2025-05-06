2076ba95f30176d4dbd50169c073484a61fb4b59
Tren Philipsen <tren.philipsen@student.fontys.nl>
2025-05-06 14:01:58 +0200

    sonarqube update

---
+++ SongService/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO song (id, title, artist, file_url) VALUES
+                                                   (1, 'Lose Yourself', 'Eminem', 'https://storageaccount.blob.core.windows.net/songs/eminem-loseyourself.mp3'),
+                                                   (2, 'Bohemian Rhapsody', 'Queen', 'https://storageaccount.blob.core.windows.net/songs/queen-bohemian.mp3'),
+                                                   (3, 'Blinding Lights', 'The Weeknd', 'https://storageaccount.blob.core.windows.net/songs/weeknd-blinding.mp3');
