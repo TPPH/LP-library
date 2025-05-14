d34ae4326d13d32e8166d40203d3efc7fc95a856
Tren Philipsen <tren.philipsen@student.fontys.nl>
2025-05-14 13:05:45 +0200

    test commit without tag

--- StreamingService/src/main/java/com/example/streamingservice/Controller/AudioController.java
+++ StreamingService/src/main/java/com/example/streamingservice/Controller/AudioController.java
@@ -5,8 +5,10 @@ import com.example.streamingservice.Service.AudioStorageService;
 import com.example.streamingservice.Service.AudioStreamingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 @RestController
 @RequestMapping("/api/audio")
@@ -20,14 +22,14 @@ public class AudioController {
 
     // Upload audio file
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadAudio(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadAudio(@RequestParam("file") MultipartFile file, @AuthenticationPrincipal Jwt jwt) {
         String fileUrl = audioStorageService.uploadFile(file);
         return ResponseEntity.ok("File uploaded successfully: " + fileUrl);
     }
 
     // Stream audio file
     @GetMapping("/stream/{fileName}")
-    public ResponseEntity<byte[]> streamAudio(@PathVariable String fileName) {
+    public ResponseEntity<byte[]> streamAudio(@PathVariable String fileName, @AuthenticationPrincipal Jwt jwt) {
         return audioStreamingService.streamAudio(fileName);
     }
 
